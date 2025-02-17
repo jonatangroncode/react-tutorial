31d094c69bfba458a5b0057d8af494d1b25414b3
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-02-17 23:28:58 +0100

    feat: -Passing function via props -Passing children -created button component -functionality to show and close alert whit buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import ListGroup from "./components/ListGroup";
-function App() {
-  const items = [
-    "new york",
-    "los angeles",
-    "chicago",
-    "houston",
-    "philadelphia",
-    "phoenix",
-  ];
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
+function App() {
+  const [alertVisable, setAlertVisibility] = useState(false);
   return (
     <>
-      <ListGroup items={items} heading="cities " />{" "}
+      {alertVisable && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </>
   );
 }
